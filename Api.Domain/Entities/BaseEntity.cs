ba3d9617f577f50dcb37fda97b6631edba31fb86
valentimhelio <valentimhelio@gmail.com>
2025-04-14 15:58:30 -0400

    Api.Domain - Implementação BaseEntity

---
+++ Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        private DateTime _creatAt;
+
+        public DateTime CreateAt
+        {
+            get { return _creatAt; }
+            set { _creatAt = (value.Equals(null) ? DateTime.UtcNow : value); }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+    }
+}
