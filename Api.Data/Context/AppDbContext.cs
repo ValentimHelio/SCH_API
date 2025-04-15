21fda1fa78c91442a6a038c4bf2fe8c1e6faa514
valentimhelio <valentimhelio@gmail.com>
2025-04-15 13:58:22 -0400

    56.Api.Data - Criar MyContext e Referenciar Api.Data com Api.Domain

---
+++ Api.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserEntitiy> Users { get; set; }
+        public DbSet<ClienteEntity> Clientes { get; set; }
+        public DbSet<EmpresaEntity> Empresas { get; set; }
+        public DbSet<MovimentoEntity> Movimentos { get; set; }
+        public DbSet<ServicoEntity> Servicos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
