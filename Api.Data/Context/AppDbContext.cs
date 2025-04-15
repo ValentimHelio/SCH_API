850c14f3a5c7ad7289d90b49e78d0c31976277b7
valentimhelio <valentimhelio@gmail.com>
2025-04-15 14:16:47 -0400

    57. Api.Data - Criar ContextFactory

--- Api.Data/Context/AppDbContext.cs
+++ Api.Data/Context/AppDbContext.cs
@@ -1,21 +1,20 @@
 ﻿using Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Api.Data.Context
+namespace Api.Data.Context;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public DbSet<UserEntitiy> Users { get; set; }
+    public DbSet<ClienteEntity> Clientes { get; set; }
+    public DbSet<EmpresaEntity> Empresas { get; set; }
+    public DbSet<MovimentoEntity> Movimentos { get; set; }
+    public DbSet<ServicoEntity> Servicos { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DbSet<UserEntitiy> Users { get; set; }
-        public DbSet<ClienteEntity> Clientes { get; set; }
-        public DbSet<EmpresaEntity> Empresas { get; set; }
-        public DbSet<MovimentoEntity> Movimentos { get; set; }
-        public DbSet<ServicoEntity> Servicos { get; set; }
-
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+        base.OnModelCreating(modelBuilder);
     }
 }
---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context;
+
+public class ContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = "Server=localhost;DataBase=SHC_API_DB;Uid=valentim;Pwd=123456";
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
